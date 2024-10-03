5bb8f56ce7c3e9e2f0278220c378ddc35310fea2
GuiPM001 <guilhermepimentelm0@gmail.com>
2024-10-02 21:44:44 -0300

    fix api service

--- spaced-repetition-front/src/services/cards.service.ts
+++ spaced-repetition-front/src/services/cards.service.ts
@@ -4,8 +4,9 @@ import { Card } from "../types/Card";
 const BASE_PATH = "http://localhost:3300";
 
 export const fetchCardsToReview = async (): Promise<AxiosResponse<Card[]>> => {
-  const response = await axios(`${BASE_PATH}/cards-to-review`, {
+  const response = await axios(`${BASE_PATH}/card/cards-to-review`, {
     method: "GET",
+    headers: { "Content-Type": "application/json"}
   });
 
   return response;
@@ -14,14 +15,15 @@ export const fetchCardsToReview = async (): Promise<AxiosResponse<Card[]>> => {
 export const saveNewCard = async (frontText: string, backText: string) => {
   var card = { front: frontText, back: backText };
 
-  await axios(`${BASE_PATH}/cards`, {
+  await axios(`${BASE_PATH}/card`, {
     method: "POST",
+    headers: { "Content-Type": "application/json"},
     data: JSON.stringify(card),
   });
 };
 
 export const reviewCard = async (id: number, optionChoose: number) => {
-  await axios(`${BASE_PATH}/review-card/id/${id}/next-review/${optionChoose}`, {
+  await axios(`${BASE_PATH}/card/review-card/id/${id}/next-review/${optionChoose}`, {
     method: "POST",
   });
 }
